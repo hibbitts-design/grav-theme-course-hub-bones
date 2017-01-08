38531b60b57f6dce219b75f92aaa69f04f15be4d
Paul Hibbitts <paul@hibbittsdesign.org>
2017-01-08 08:45:54 -0800

    Added styling to HTML alerts.

--- course-hub-bones.php
+++ course-hub-bones.php
@@ -8,12 +8,12 @@ class CourseHubBones extends Theme
   public function onThemeInitialized() {
     // Check for External_Links plugin status
     if ($this->grav['config']->get('plugins.external_links.built_in_css')) {
-      echo 'Please set the Use Built-in CSS option for the External Links plugin to No (i.e. False) before using this theme.';
+      echo '<div class="callout warning" style="margin-bottom: 0;">Please set the Use Built-in CSS option for the External Links plugin to No (i.e. False) before using this theme.</div>';
       return;
     }
     // Check for Bootstrapper plugin status
     if ($this->grav['config']->get('plugins.bootstrapper.enabled')) {
-      echo 'Please disable the Bootstrapper plugin before using this theme.';
+      echo '<div class="callout warning" style="margin-bottom: 0;">Please disable the Bootstrapper plugin before using this theme.</div>';
     }
   }
 }
